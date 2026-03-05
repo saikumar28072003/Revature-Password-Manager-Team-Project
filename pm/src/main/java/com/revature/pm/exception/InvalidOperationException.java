130d2cccb150aebf303d46f4242b46e636f1caab
Md Saleem <87407229+MD-Saleem-07@users.noreply.github.com>
2026-03-05 11:07:03 +0530

    Delete pm directory
    By mistake Pushed

--- pm/src/main/java/com/revature/pm/exception/InvalidOperationException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.pm.exception;
-
-public class InvalidOperationException extends RuntimeException {
-
-    public InvalidOperationException(String message) {
-        super(message);
-    }
-}
--- pm/src/main/java/com/revature/pm/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.pm.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
